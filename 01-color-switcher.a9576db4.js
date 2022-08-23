018bb1338d9321c0b4677051c53c4d7332e94298
natikagrin <99361467+natikagrin@users.noreply.github.com>
2022-08-23 20:42:50 +0000

    Deploying to gh-pages from @ natikagrin/goit-js-hw-09@2e8f74c8f43762ad17f94818b50a8446e78b5927 🚀

---
+++ 01-color-switcher.a9576db4.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");let a=null;function d(){document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(t){a=setInterval(d,1e3),t.target.disabled=!0,e.disabled=!1})),e.addEventListener("click",(function(e){clearInterval(a),e.target.disabled=!0,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.a9576db4.js.map
